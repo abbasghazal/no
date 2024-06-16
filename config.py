1b09f919268e4da8f78e6127368348f31949f6eb
abbasghazal <134511359+abbasghazal@users.noreply.github.com>
2024-06-16 20:43:30 +0300

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Heroku-Back/Red-Wine_Music",
+    "https://github.com/abbasghazal/music2",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
